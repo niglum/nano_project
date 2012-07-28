348c5736ae98a55bb2a74f99fd5b127ee70501a9
niglum <arthur.wutt@gmail.com>
2012-07-28 13:36:55 +0300

    Update src/MainClass.java

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,6 +4,7 @@ public class MainClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("start");
 		Bank bank = new Bank(1000);
 		bank.getSumm();
 //i have no idea what i doing
